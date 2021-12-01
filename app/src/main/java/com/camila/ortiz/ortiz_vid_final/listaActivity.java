5888673dc212cd11b1c6f8cf1a5b12f7495a4e6b
Camila Ortiz <cami089844@gmil.com>
2021-12-01 10:20:40 -0600

    fianl

---
+++ app/src/main/java/com/camila/ortiz/ortiz_vid_final/listaActivity.java
@@ -0,0 +1,14 @@
+package com.camila.ortiz.ortiz_vid_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista);
+    }
+}
